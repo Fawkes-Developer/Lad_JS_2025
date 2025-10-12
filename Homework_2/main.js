e51bc7116a0c7e32f9c0dddb353f357db56359b3
Dmitriy Fyodorov <fawkes.developer@gmail.com>
2025-10-12 23:20:29 +0700

    feat: add changes in main.js

---
+++ Homework_2/main.js
@@ -0,0 +1,41 @@
+// Задание 3.1
+const userAge = 18;
+
+// Задание 3.2
+let isAuthorized = false;
+
+// Задание 3.3
+let cart = [ 
+  { name: "Product1", count: 2, cost: 1000, },
+  { name: "Product2", count: 4, cost: 300, },
+  { name: "Product3", count: 1, cost: 2500, },
+];
+
+cart.forEach(product => console.log(product));
+
+// Задание 3.4
+const book = {
+  name: "You don't know JS",
+  author: "Kyle Simpson",
+  year: 2015,
+  pages: 88,
+}; 
+
+// Задание 3.5
+let discountedTotalCost = 1000;
+
+// Задание 3.6
+const timerId = 22;
+
+// Задание 3.7
+const openModalBtn = null;
+
+// Задание 3.8
+let isOpenPopup = false;
+
+// Задание 3.9
+let userName = "Игорь";
+let userGreeting = `"Добро пожаловать, ${userName}"`;
+
+// Задание 3.10
+SubmitRegistrationForm => console.log("Отправляю форму!"); 
